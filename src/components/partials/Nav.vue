f7a98144e0b7e29ca2090142bc4cde3168336902
enrico-amerio <amerio.enrico95@gmail.com>
2024-04-08 16:13:35 +0200

    first commit

---
+++ src/components/partials/Nav.vue
@@ -0,0 +1,78 @@
+<script>
+import {store} from '../../data/store'
+  export default {  
+    data(){
+      return{
+        store
+      }
+    },
+    
+  }
+</script>
+
+
+<template>
+  <nav class="navbar navbar-expand-lg">
+  <div class="container-fluid">
+    <div class="col-6">
+      <a class="navbar-brand" href="#">
+        <img src="../../assets/img/logo-img-01.png" alt="Everlead-logo" class="logo">
+      </a>
+    </div>
+    <div class="col-6">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0  ">
+          <li class="nav-item">
+            <a class="nav-link" href="#">HOMES</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">PAGES</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">BLOG</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">SHOP</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">EVENTS</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">ELEMENTS</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"><i class="fa-solid fa-magnifying-glass"></i></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"><i class="fa-solid fa-list-ul"></i></a>
+          </li>
+        </ul>
+      </div>
+      
+    </div>
+  </div>
+</nav>
+</template>
+
+
+
+<style lang="scss" scoped>
+nav{
+  position:fixed;
+  z-index: 999;
+  width: 100%;
+    .logo{
+      width: 100px;
+    }
+    .nav-link{
+      font-size:10px;
+      font-weight: 700;
+      &:hover{
+        color: orange;
+      }
+    }
+}
+</style>
